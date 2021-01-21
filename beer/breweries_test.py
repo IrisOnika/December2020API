5a52c09e56ee192459c27df0089f26ae27935ed4
IrisOnika <IrisOnika@yandex.ru>
2021-01-21 10:31:45 +0300

    небольшая доработка + коммент о возможном баге в автокомплите =)

--- beer/breweries_test.py
+++ beer/breweries_test.py
@@ -12,6 +12,9 @@ def test_check_breweries_schema(number):
     v = cerberus.Validator(schema)
     assert v.validate(res)
 
+
+# проверка количества пивоварен на странице по умолчанию.
+# А также проверка общего кол-ва пивоварен на текущее состояние базы
 @pytest.mark.parametrize(('page_num', 'count'),
                          [
                              # кейсы для номера вызываемой страницы
@@ -31,6 +34,7 @@ def test_check_brewer_page_num(page_num, count):
     assert res_count == count
 
 
+# проверка возможности выводить разное кол-во пивоварен на странице
 @pytest.mark.parametrize(('per_page', 'count'), [('0', 0),
                                       ('1', 1),
                                       ('50', 50),
@@ -44,23 +48,24 @@ def test_check_brewer_per_page(per_page, count):
     assert res_count == count
 
 
-@pytest.mark.parametrize('search', ['California', 'Russia', 'Fat Orange Cat', 'khjkhjkh'])
+@pytest.mark.parametrize('search', ['California', 'Russia', 'Fat Orange Cat', 'Cat', 'khjkhjkh'])
 def test_check_brewer_search(search):
     ap = brewer_api()
     res = ap.text_dict(path=f'{search_query}{search}')
    # print(f'res={res}')
-    # Идея теста проверять, что если пивоварня нашлась по какому-либо набору символов, то этот набор символов попадёт
+    # Идея теста - проверять, что если пивоварня нашлась по какому-либо набору символов, то этот набор символов попадёт
     # в строку вывода(в одно из полей).
-    # по факту работает не со всеми значениями, но, возможно, я некорректно разобралась в работе сервиса.
+    # по факту работает не со всеми значениями ('Cat'), но, возможно, я некорректно разобралась в работе сервиса.
     for item in res:
         print(item.get("id"))
         count=0
         for i in list(item.keys()):
+            # добавить проверку на некорректную обработку (если подстрока нигде не нашлась.)
             if str(item.get(i)).find(search) > -1:
                 print(i)
                 print(item.get(i))
                 count += 1
-                print(count)
+                #print(count)
         if res != []:
             assert count > 0
         else:
@@ -68,15 +73,18 @@ def test_check_brewer_search(search):
 
 
 # тест про схему ответа при автокомплите
-@pytest.mark.parametrize('search', ['Fat Orange Cat'])
+# Баг (или фича). id в выводе имеет тип 'string'. id в других методах имеет тип 'integer'
+@pytest.mark.parametrize('search', ['Fat Orange Cat', 'Mew'])
 def test_autocomplete_schema(search):
     ap = brewer_api()
-    res = ap.text_dict(path=f'{autocomplete_query}{search}')[0]
-    print(res)
+    res = ap.text_dict(path=f'{autocomplete_query}{search}')
+   # print(res)
     schema = open_read('beer/schema_autocomplete.json')
-    print(schema)
+   # print(schema)
     v = cerberus.Validator(schema)
-    assert v.validate(res)
+    for res_item in res:
+        assert v.validate(res_item)
+
 
 
 
