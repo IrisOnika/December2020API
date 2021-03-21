70e78e065d693852f27209a4457c47d3dd6f8b3e
IrisOnika <IrisOnika@yandex.ru>
2021-03-21 21:28:11 +0300

    обновление тестов про породы собак (перевод на фикстуры + корректировка данных в связи с их обновлением в источнике)

--- dogs/breed_list_func.py
+++ dogs/breed_list_func.py
@@ -1,4 +1,5 @@
 from conftest import api_client_base
+import pytest
 
 
 host = 'https://dog.ceo/api/breeds'
@@ -8,6 +9,13 @@ random1 = '/image/random'
 random1_result = 'https://images.dog.ceo/breeds/'
 
 
+@pytest.fixture()
 def dogs_api(path=host):
+    ap = api_client_base(path)
+    return ap
+
+
+@pytest.fixture()
+def dog_api(path=host1):
     ap = api_client_base(path)
     return ap
--- dogs/dogs_test.py
+++ dogs/dogs_test.py
@@ -4,48 +4,44 @@ import pytest
 import re
 
 
-def test_check_breeds_list():
+def test_check_breeds_list(dogs_api):
     """проверка возвращаемого списка пород"""
-    ap = dogs_api()
-    res = ap.text_dict(path=list_all)
+    res = dogs_api.text_dict(path=list_all)
     breed_list_dict = res.get("message")
     breeds_list = open_read('dogs/breеd_list.json')
     assert breed_list_dict == breeds_list
 
 
-def test_breed_random():
+def test_breed_rand(dogs_api):
     """проверка метода random - возвращает одну рандомную фотографию"""
-    ap = dogs_api()
-    res = ap.text_dict(path=random1)
+    res = dogs_api.text_dict(path=random1)
     rand1 = res.get("message")
     # Проверяем, что в message одна строка в виде "https://images.dog.ceo/breeds/{название_породы}/{номер_картинки}.jpg"
     assert re.match(r"^" + random1_result + r"\S+/\S+.[jpegJPEG]{3,4}$", rand1)
 
 
 @pytest.mark.parametrize("count", ['1', '11', '50', '51', '0', 's'])
-def test_breed_links_random(count):
+def test_breed_links_random(dogs_api, count):
     """проверка множественного random'а - каждая возвращённая строка - именно фотография"""
-    ap = dogs_api()
-    res = ap.text_dict(path=random1+'/'+count)
+    res = dogs_api.text_dict(path=random1+'/'+count)
     links = res.get("message")
     for link in links:
         assert re.match(r"^" + random1_result + r"\S+/\S+.[jpegJPEG]{3,4}$", link)
 
 
 @pytest.mark.parametrize(("count", "result"), [('1', 1), ('11', 11), ('50', 50), ('51', 50), ('0', 1), ('s', 1)])
-def test_breed_links_count(count, result):
+def test_breed_links_count(dogs_api, count, result):
     """проверка множественного random'а - возвращает заданное кол-во ссылок + граничные и "некорректные" значения"""
-    ap = dogs_api()
-    res = ap.text_dict(path=random1 + '/' + count)
+    res = dogs_api.text_dict(path=random1 + '/' + count)
     links = res.get("message")
     assert len(links) == result
 
 
-@pytest.mark.parametrize("breed", ['setter'])
-def test_breed_links_images(breed):
+@pytest.mark.parametrize("breed", ['setter', 'terrier'])
+def test_breed_links_images(dog_api, breed):
     """проверка метода с указанием породы"""
-    ap = dogs_api(host1)
-    res = ap.text_dict(path='/' + breed + '/images')
+    res = dog_api.text_dict(path='/' + breed + '/images')
     links = res.get("message")
     for link in links:
-        assert re.match(r"^" + random1_result + breed + r"\S+/\S+.[jpegJPEG]{3,4}$", link)
+        print(f'link={link}')
+        assert re.match(r"^" + random1_result + breed + r"\S+/\S+.[jpegJPEGn]{3,4}$", link)
